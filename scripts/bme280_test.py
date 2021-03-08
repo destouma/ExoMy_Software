4cd5c5c0254597b602b09b0a64ec8d117d66d896
Manuel Destouesse <mdsoftengr@gmail.com>
2021-03-08 14:40:19 -0500

    WIP BME280

--- scripts/bme280_test.py
+++ scripts/bme280_test.py
@@ -7,8 +7,9 @@ class sensorBME280():
     bus = smbus2.SMBus(port)
 
     def getValue(self):
-        calibration_params = bme280.load_calibration_params(bus, address)
-        data = bme280.sample(bus, address, calibration_params)
+        calibration_params = bme280.load_calibration_params(self.bus, self.address)
+        data = bme280.sample(self.bus, self.address, calibration_params)
+	return data
 
 sensor = sensorBME280()
 data = sensor.getValue()
